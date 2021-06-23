5fb77c862af35ce47e65cac48c9ac61bd371fa37
codpoe <codpoe.me@gmail.com>
2021-06-24 00:34:45 +0800

    fix: put react-helmet in external to improve ssr

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,13 @@ export default defineConfig(() => {
         // 这里需要把 @mdx-js/react 设为 external，不能打包进 bundle，
         // 因为 markdown 文件经过 mdx 转换生成的代码依赖了 node_modules 的 @mdx-js/react，
         // 如果打包进 bundle，会导致 MDXContext 不是同一个了
-        external: ['react', 'react-dom', 'react-router-dom', '@mdx-js/react'],
+        external: [
+          'react',
+          'react-dom',
+          'react-router-dom',
+          '@mdx-js/react',
+          'react-helmet',
+        ],
       },
     },
     plugins: [windicss()],
